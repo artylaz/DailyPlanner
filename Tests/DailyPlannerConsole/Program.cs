3df776e4c9ddb10c78399123c30ecf14c1b79783
arty-laz@mail.ru <arty-laz@mail.ru>
2020-11-24 03:44:34 +0600

    Инфраструктура папок готова

---
+++ Tests/DailyPlannerConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyPlannerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
